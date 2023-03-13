455510e16757be4b4fd4521a2c8a85d5b3634870
Alex <riedlial@web.de>
2023-03-13 14:32:54 +0100

    Added docstrings

--- undoc_chat_client.py
+++ undoc_chat_client.py
@@ -1,19 +1,3 @@
-"""Module implementing a chat client.
-
-This module provides the implementation of a chat client that can connect to a
-server and enable communication with other clients also connected to the same server.
-
-Usage example:
-
-    $ python chat_client.py <server_ip>
-
-Attributes:
-    MAX_MSG_LENGTH (int): The maximum length of a message that can be sent.
-
-Authors:
-    Alexander Riedlinger <alexander.riedlinger@student.dhbw-vs.de>
-"""
-
 import socket
 import sys
 import threading
@@ -23,54 +7,16 @@ MAX_MSG_LENGTH = 126
 
 
 class ChatClient:
-    """
-    A class representing a client for a chat application.
-
-    Args:
-    server_ip (str): The IP address of the server.
-
-    Attributes:
-    server_ip (str): The IP address of the server.
-    server_port (int): The port number of the server.
-    client_socket (socket.socket): The socket object representing the client's
-    connection to the server.
-    client_id (str): The ID of the client.
-    response_queue (Queue): A queue to store responses from the server.
-    stop_event (threading.Event): An event to signal the response handling
-    thread to stop.
-    response_thread (threading.Thread): A thread to handle server messages.
-
-    Methods:
-    register(client_id): Registers the client with the server using the provided
-    client ID.
-
-    """
     def __init__(self, server_ip):
-        """
-        Initializes a new instance of the ChatClient class.
-
-        Args:
-        server_ip (str): The IP address of the server.
-        """
         self.server_ip = server_ip
         self.server_port = 2900
         self.client_socket = None
         self.client_id = None
         self.response_queue = Queue()
         self.stop_event = threading.Event()
-        self.response_thread = threading.Thread(target=self.handle_server_message,
-                                                daemon=True)
+        self.response_thread = threading.Thread(target=self.handle_server_message, daemon=True)
 
     def register(self, client_id):
-        """
-        Registers the client with the server using the provided client ID.
-
-        Args:
-        client_id (str): The ID to register the client with.
-
-        Returns:
-        bool: True if the registration is successful, False otherwise.
-        """
         try:
             if not self.client_socket:
                 self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -91,17 +37,10 @@ class ChatClient:
                 print(f"{response}")
                 return False
         except (TimeoutError, ConnectionError):
-            print("ERROR: Connection to server failed. Make sure the server is "
-                  "running and try again.")
+            print("ERROR: Connection to server failed. Make sure the server is running and try again.")
             sys.exit(1)
 
     def handle_server_message(self):
-        """
-        Continuously listens for messages from the server and puts them in a
-        response queue.
-        If a "SHUTDOWN" message is received, sets the stop event, disconnects
-        from the server, and exits the loop.
-        """
         while not self.stop_event.is_set():
             try:
                 # Set a timeout of 1 second
@@ -112,8 +51,7 @@ class ChatClient:
 
                 if message == "SHUTDOWN":
                     print(
-                        "\n!!! You have been disconnected from the server because"
-                        " it has been shut down. Press any key to exit.!!!")
+                        "\n!!! You have been disconnected from the server because it has been shut down. Press any key to exit.!!!")
                     self.stop_event.set()
                     self.disconnect()
                     break
@@ -123,38 +61,21 @@ class ChatClient:
                 # If a timeout occurs, just continue the loop
                 continue
             except ConnectionResetError:
-                print(
-                    "\n!!! Unexpectedly lost connection to server. "
-                    "Press any key to exit.!!!")
+                print("\n!!! Unexpectedly lost connection to server. Press any key to exit.!!!")
                 self.stop_event.set()
                 break
 
     def list_other_clients(self):
-        """Sends a request to the server to list all other clients currently logged in.
-
-        Prints a formatted list of the other clients' IDs.
-        """
         request = "LIST"
         self.client_socket.send(request.encode())
         response = self.response_queue.get()
         print(f"Other clients logged in:\n{response}")
 
     def send_message(self, recipient, message):
-        """Sends a message to the specified recipient.
-
-        Args:
-            recipient (str): The ID of the client to send the message to.
-            message (str): The message to send.
-
-        """
         request = f"SEND {recipient} {message}"
         self.client_socket.send(request.encode())
 
     def check_messages(self):
-        """Sends a request to the server to check for new messages.
-
-        Prints any new messages that have been received since the last check.
-        """
         request = "CHECK"
         self.client_socket.send(request.encode())
         response = self.response_queue.get()
@@ -164,40 +85,17 @@ class ChatClient:
             print(response)
 
     def disconnect(self):
-        """
-        Disconnect from the server by sending a "DISCONNECT" request and setting
-        the stop_event flag.
-        """
         self.stop_event.set()
         request = "DISCONNECT"
         self.client_socket.send(request.encode())
 
     def quit(self):
-        """
-        Disconnect from the server and print any old messages before exiting
-        the client.
-        """
         print("Old messages:")
         self.check_messages()
         print(f"Closing down Session.\nGoodbye {self.client_id}!")
         self.disconnect()
 
     def run(self):
-        """
-        Starts the chat client and listens for user input to interact with the
-        chat server.
-
-        The method first prompts the user to select a client ID and attempts to
-        register with the server using this ID.
-        If the registration is successful, the method starts a separate thread
-        to listen for incoming server messages.
-
-        The method then enters a loop to process user input. The user can choose
-        to list other logged-in clients, send a message to another client, check
-        for incoming messages, or quit the chat system. Invalid selections will
-        result in an error message.
-
-        """
         while True:
             cid = input("Choose client ID: ").strip()
             if self.register(cid):
@@ -205,42 +103,40 @@ class ChatClient:
 
         # Start a separate thread to listen for server messages
         self.response_thread.start()
-
         while not self.stop_event.is_set():
             print("====== Minimal Chat System ======")
             print("1: other Clients logged in")
             print("2: Send message")
             print("3: Check incoming messages")
             print("4: Quit")
-
             selection = input("Your selection: ")
 
             if self.stop_event.is_set():
                 break
             else:
                 if selection == "1":
-                    self.list_other_clients()
+                    chat_client.list_other_clients()
                 elif selection == "2":
                     recip = input("Send message to: ")
-                    if recip == self.client_id:
+                    if recip == chat_client.client_id:
                         print("ERROR: You cannot send a message to yourself.")
                     else:
                         msg = input("Your message: ")
                         if len(msg) > MAX_MSG_LENGTH:
                             print("ERROR: Message too long, please limit to 126 characters")
                         else:
-                            self.send_message(recip, msg)
+                            chat_client.send_message(recip, msg)
                 elif selection == "3":
-                    self.check_messages()
+                    chat_client.check_messages()
                 elif selection == "4":
-                    self.quit()
+                    chat_client.quit()
                 else:
                     print("Invalid selection. Please try again.")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python chat_client.py <server_ip>")
+        print("Usage: python undoc_chat_client.py <server_ip>")
         sys.exit(1)
 
     serv_ip = sys.argv[1]
--- undoc_chat_server.py
+++ undoc_chat_server.py
@@ -1,19 +1,3 @@
-"""Module implementing a chat server.
-
-This module defines the ChatServer class, which can be used to start a chat
-server that allows multiple clients to connect and communicate with each other.
-
-Usage example:
-
-    with ChatServer(server_ip, server_port) as server:
-        server.start()
-
-    $ python chat_server.py
-
-Authors:
-    Alexander Riedlinger <alexander.riedlinger@student.dhbw-vs.de>
-"""
-
 import socket
 import sys
 from threading import Thread, Lock
@@ -23,29 +7,7 @@ import time
 
 
 class ChatServer:
-    """A simple chat server that allows multiple clients to connect and
-    communicate with each other.
-
-    Attributes:
-        ip_addr (str): The IP address of the server.
-        port (int): The port number to use for the server.
-        clients (dict): A dictionary containing the clients currently connected
-        to the server. The keys are client IDs and the values are socket objects.
-        lock (threading.Lock): A lock used to ensure thread safety.
-        message_queue (collections.defaultdict): A dictionary containing the
-        message queues for each connected client. The keys are client IDs and
-        the values are queue.Queue objects.
-        running (bool): A flag indicating whether the server is currently running.
-    """
-
     def __init__(self, ip_addr=socket.gethostbyname(socket.gethostname()), port=2900):
-        """Initialize a new ChatServer object.
-
-        Args:
-            ip_addr (str): The IP address of the server (default is the local
-            machines IP address).
-            port (int): The port number to use for the server (default is 2900).
-        """
         self.ip_addr = ip_addr
         self.port = port
         self.clients = {}
@@ -54,12 +16,6 @@ class ChatServer:
         self.running = False
 
     def __enter__(self):
-        """
-        Start the server when entering a "with" block.
-
-        Returns:
-            ChatServer: The ChatServer object.
-        """
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.server_socket.bind((self.ip_addr, self.port))
@@ -74,14 +30,6 @@ class ChatServer:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        """
-        Stop the server when exiting a "with" block.
-
-        Args:
-            exc_type: The exception type (not used).
-            exc_value: The exception value (not used).
-            traceback: The traceback (not used).
-        """
         self.running = False
         for client_socket in self.clients.values():
             try:
@@ -91,16 +39,12 @@ class ChatServer:
         self.server_socket.close()
 
     def start(self):
-        """
-        Start the server and handle incoming client connections.
-        """
         threads = []
         try:
             while self.running:
                 try:
                     client_socket, address = self.server_socket.accept()
-                    client_thread = Thread(target=self.handle_client,
-                                           args=(client_socket, address))
+                    client_thread = Thread(target=self.handle_client, args=(client_socket, address))
                     client_thread.start()
                     threads.append(client_thread)
                 except socket.timeout:
@@ -113,12 +57,10 @@ class ChatServer:
                 thread.join()
 
     def stop(self):
-        """
-        Stop the server and disconnect all clients.
-        """
         self.running = False
         countdown = 5
 
+        # Send a SHUTDOWN message to each client and wait for them to disconnect
         with self.lock:
             for client_id, client_socket in self.clients.items():
                 try:
@@ -126,6 +68,7 @@ class ChatServer:
                 except Exception as e:
                     print(f"Error sending SHUTDOWN to client '{client_id}': {e}")
 
+            # Wait for the specified time, displaying a countdown on the server console
             while countdown > 0:
                 print(f"Server shutting down in {countdown} seconds...")
                 time.sleep(1)
@@ -134,19 +77,11 @@ class ChatServer:
             # Make sure everything is closed properly
             for client_socket in self.clients.values():
                 with client_socket:
-                    pass
+                    pass  # Send nothing, just close the socket
             with self.server_socket:
-                pass
+                pass  # Send nothing, just close the socket
 
     def handle_client(self, client_socket, address):
-        """Handle messages from a connected client.
-
-        Args:
-            client_socket (socket.socket): The socket object representing the
-            client connection.
-            address (tuple): A tuple containing the client's IP address and
-            port number.
-        """
         with client_socket:
             while True:
                 client_id = client_socket.recv(1024).decode()
@@ -155,8 +90,7 @@ class ChatServer:
 
                 with self.lock:
                     if client_id in self.clients:
-                        client_socket.send(b"ERROR: Client ID already taken. "
-                                           b"Please choose another one.")
+                        client_socket.send(b"ERROR: Client ID already taken. Please choose another one.")
                     else:
                         self.clients[client_id] = client_socket
                         print(f"Client '{client_id}' connected from {address}\n")
@@ -184,11 +118,9 @@ class ChatServer:
 
                     elif command == "LIST":
                         with self.lock:
-                            other_clients = [cid for cid in self.clients if cid
-                                             != client_id]
+                            other_clients = [cid for cid in self.clients if cid != client_id]
                             if len(other_clients) != 0:
-                                client_socket.sendall("\n".join(other_clients)
-                                                      .encode())
+                                client_socket.sendall("\n".join(other_clients).encode())
                             else:
                                 client_socket.sendall(b"Only you at the moment!")
 
